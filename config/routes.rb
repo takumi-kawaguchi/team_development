cad4bb69cfa096b99ba2b479ffff0ca923a9ab1f
EC2 Default User <ec2-user@ip-172-31-8-231.us-east-2.compute.internal>
2018-04-24 04:56:07 +0000

    routes.rbを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-
   devise_for :users
   get 'pages/index'
   get 'pages/show'
 
   resources :books, only: [:index, :new, :create]
-
+  
   root to: 'home#index'
   
 end
