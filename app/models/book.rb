5058a8b13dee86effa4db048287974c5d5a6d767
arisa-kusano <arisakusano1227@gmail.com>
2018-04-24 05:59:21 +0000

    本のデータ検証の追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,9 @@
 class Book < ApplicationRecord
+
+  # データ検証(入力に制限をかけるなど)：validation
+  # presence：必須
+  validates :title, :author, presence: true
+  # length {}：文字数制限
+  validates :title, :author, :publisher, length: { maximum: 100}
+
 end
