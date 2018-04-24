86050363f021928d6f30f41d63464f676fcd191d
arisa-kusano <arisakusano1227@gmail.com>
2018-04-24 07:01:01 +0000

    本のデータ入力における制限の追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,5 +5,16 @@ class Book < ApplicationRecord
   validates :title, :author, presence: true
   # length {}：文字数制限
   validates :title, :author, :publisher, length: { maximum: 100}
+  validate :validates_past_published_date
+  
+  private
+  
+  def validates_past_published_date
+    if self.published_date.present?
+      if Time.zone.today < self.published_date
+        self.errors.add :published_date, :invalid
+      end
+    end
+  end
 
 end
