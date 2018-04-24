982b285ae14531bd38ee5d9b9596047e20e31460
cheebacca <k.chiba.dev@gmail.com>
2018-04-24 06:04:29 +0000

    review#newのview実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+
+  get 'reviews/create'
+
+  get 'reviews/show'
+
+  get 'reviews/index'
+
+  get 'reviews/destroy'
+
+  get 'reviews/edit'
+
+  get 'reviews/update'
+
   devise_for :users
   get 'pages/index'
   get 'pages/show'
@@ -8,6 +22,7 @@ Rails.application.routes.draw do
   get 'home/welcome'
 
   resources :users
+  resources :reviews
   root to: 'home#index'
   
 end
