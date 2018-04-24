db77aa972c65e48163cd6e460fa645ea499d4dde
takumi-kawaguchi <tcgds4@gmail>
2018-04-24 06:52:54 +0000

    reviewの一覧、詳細、投稿機能実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,25 @@
 class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.where(user_id: current_user.id).all
+  end
+  
+  def show
+    
+  end
+  
   def new
     @review = Review.new
   end
 
   def create
-    
-  end
-
-  def show
-  end
-
-  def index
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.book_id = Book.first.id
+    if @review.save
+      redirect_to @review, notice: 'レビューの作成に成功しました。'
+    else
+       render action: :new, notice: "sippai"
+    end
   end
 
   def destroy
@@ -20,5 +29,11 @@ class ReviewsController < ApplicationController
   end
 
   def update
+  end
+  
+  private
+  
+  def review_params
+    params.require(:review).permit(:title, :context, :rating, :user_id)
   end
 end
